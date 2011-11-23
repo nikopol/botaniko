02b6bcc1ea2b6270bb6cad619b39fd628577d28c
niko <nikomomo@gmail.com>
2011-11-23 17:43:13 +0100

    wikipedia command added

---
+++ lib/botaniko/plugin/wikipedia.pm
@@ -0,0 +1,47 @@
+package botaniko::plugin::wikipedia;
+
+use strict;
+use warnings;
+use 5.010;
+
+use HTML::Entities;
+use JSON::XS;
+
+use botaniko::config;
+use botaniko::logger;
+use botaniko::command;
+use botaniko::tools;
+
+cfg_default 'plugins.wikipedia' => {
+	url => "http://%s.wikipedia.org/w/api.php?action=query&list=search&srlimit=1&srsearch=%s&format=json",
+	loc => "en",
+};
+
+command wikipedia => {
+	help => "wikipedia query [lang=fr]",
+	bin  => sub {
+		my( $qry, $loc ) = getoptions(\@_,'lang');
+		$qry = join(' ',@$qry);
+		return [ 'what are you searching for ?' ] unless $qry;
+		$loc ||= cfg 'plugins.wikipedia.loc';
+		my $url = sprintf cfg('plugins.wikipedia.url'), $loc, $qry;
+		trace DEBUG=>'loading '.$url;
+		my $r = useragent->get( $url );
+		unless( $r->is_success ) {
+			my $err = $url.' returned '.$r->status_line; 
+			trace WARN=>$err;
+			return [ $err ];
+		}
+		my $w = eval { decode_json( $r->decoded_content ) }
+			or return ['error parsing answer'];
+		if( $w->{query} && $w->{query}->{search} ) {
+			my $l = $w->{query}->{search}->[0]->{snippet};
+			$l =~ s/<[^<>]+>//g;
+			$l =~ s/\s+/ /g;
+			return [ decode_entities( $l ) ];
+		}
+		[ 'nothing found' ];
+	}
+};
+
+1
